8e19a35acdf58c6b0a22781954caf12ad3021f3c
mfogut <fatihogut27@gmail.com>
2020-12-17 11:43:39 -0500

    Private-RT-2 subnet assocatiaon configured

--- private_rt.tf
+++ private_rt.tf
@@ -13,14 +13,26 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-resource "aws_route_table_association" "private_rt_association_a" {
+resource "aws_route_table_association" "private1_rt_association_a" {
   count          = length(aws_subnet.private_subnet)
   subnet_id      = aws_subnet.private_subnet[0].id
   route_table_id = aws_route_table.private_rt[0].id
 }
 
-resource "aws_route_table_association" "private_rt_association_b" {
+resource "aws_route_table_association" "private1_rt_association_b" {
   count          = length(aws_subnet.db_subnet)
   subnet_id      = aws_subnet.db_subnet[0].id
   route_table_id = aws_route_table.private_rt[0].id
+}
+
+resource "aws_route_table_association" "private2_rt_association_a" {
+  count          = length(aws_subnet.private_subnet)
+  subnet_id      = aws_subnet.private_subnet[1].id
+  route_table_id = aws_route_table.private_rt[1].id
+}
+
+resource "aws_route_table_association" "private2_rt_association_b" {
+  count          = length(aws_subnet.db_subnet)
+  subnet_id      = aws_subnet.db_subnet[1].id
+  route_table_id = aws_route_table.private_rt[1].id
 }
