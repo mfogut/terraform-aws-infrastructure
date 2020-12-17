9b767a92335a942cf6f51daba9ccbd49d9218a65
mfogut <fatihogut27@gmail.com>
2020-12-17 17:03:24 -0500

    Launch Configuration added

---
+++ launch_config.tf
@@ -0,0 +1,9 @@
+resource "aws_launch_configuration" "app_server" {
+  name_prefix       = "App-Server-"
+  image_id          = data.aws_ami.app_server.id
+  instance_type     = "t2.micro"
+  key_name          = "demo"
+  security_groups   = [aws_security_group.app_server_sg.id]
+  placement_tenancy = "default"
+  #user_data         = file(apache.sh)
+}
