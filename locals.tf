e5b34de61041e3430d03fe01c53cdc94192706f5
mfogut <fatihogut27@gmail.com>
2020-12-21 10:59:28 -0500

    DB Subnet IDs added

--- locals.tf
+++ locals.tf
@@ -3,6 +3,18 @@ locals {
 }
 
 locals {
+  public_subnets_id = aws_subnet.public_subnet.*.id
+}
+
+locals {
+  private_subnets_id = aws_subnet.private_subnet.*.id
+}
+
+locals {
+  db_subnets_id = aws_subnet.db_subnet.*.id
+}
+
+locals {
   end_index_public_subnet = 2
 }
 
@@ -12,4 +24,12 @@ locals {
 
 locals {
   end_index_db_subnet = 2
+}
+
+locals {
+  db_username = "fatih"
+}
+
+locals {
+  db_password = "fatih123"
 }
